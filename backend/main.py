eaff2b0d07d7e5f6c37003fbd9b80f5c17819828
Nikithacoco <nikitha1ga18ec037@gmail.com>
2026-08-16 10:31:21 +0530

    Fix CORS for deployed frontend

--- backend/main.py
+++ backend/main.py
@@ -101,6 +101,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174"
+        "https://healthcare-frontend-b1sg.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
