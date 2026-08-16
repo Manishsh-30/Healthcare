d470292ad9be80d0d5584331d39a0a243f4b212f
Nikithacoco <nikitha1ga18ec037@gmail.com>
2026-08-16 11:47:01 +0530

    Fix frontend CORS

--- backend/main.py
+++ backend/main.py
@@ -97,12 +97,14 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174"
-        "https://healthcare-frontend-b1sg.onrender.com",
-    ],
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
+
+    # Deployed frontend
+    "https://healthcare-frontend-b1sg.onrender.com",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
